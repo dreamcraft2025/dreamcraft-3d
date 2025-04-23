1a7929fd91f83fa4990bcba38d48523a394d663a
dreamcraft2025 <craftdream2025@gmail.com>
2025-04-23 15:27:43 -0400

    Add files via upload

--- editor/editor.js
+++ editor/editor.js
@@ -261,3 +261,24 @@ stage.on('click', (e) => {
     layer.draw();
   }
 });
+
+
+// Función para añadir un muro simple en la escena Konva
+function addWall() {
+    const wall = new Konva.Rect({
+        x: 50,
+        y: 50,
+        width: 100,
+        height: 30,
+        fill: 'gray',
+        draggable: true,
+        name: 'muro'
+    });
+
+    const layer = stage.findOne('.layer') || new Konva.Layer();
+    layer.add(wall);
+    if (!stage.findOne('.layer')) {
+        stage.add(layer);
+    }
+    layer.draw();
+}
